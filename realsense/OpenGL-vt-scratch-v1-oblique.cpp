476a058347b0a1eb235b1bd2a8dec803131ed887
hamalab.nii <hamalab.nii@outlook.com>
2025-10-13 23:41:52 +0900

    OpenGL-vt-scratch-v1-obliqueを修正

--- realsense/OpenGL-vt-scratch-v1-oblique.cpp
+++ realsense/OpenGL-vt-scratch-v1-oblique.cpp
@@ -1,6 +1,6 @@
 /*
-    OpenGL版vtをスクラッチで作成(learn-OpenGL-ch3ベースだが)
-    v1:1つのカメラで1つの点を撮影する
+	OpenGL版vtをスクラッチで作成(learn-OpenGL-ch3ベースだが)
+	v2: 斜め(Oblique)プロジェクションを実装
 */
 
 #include <glad/glad.h>
@@ -10,7 +10,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <shader_m.h>
+#include "shader_m.h"
 
 #include <opencv2/opencv.hpp>
 
@@ -31,7 +31,7 @@ int main(void) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "Oblique Projection Example", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -48,49 +48,38 @@ int main(void) {
 	}
 
 	glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
+	glEnable(GL_DEPTH_TEST);
 
+	// シェーダーは変更不要
 	Shader ourShader("shader-vt-v1.vert", "shader-vt-v1.frag");
 
 	float vertices[] = {
 		// positions         // colors
-		19.950f, 19.950f, 20.0f,  1.0f, 1.0f, 1.0f
-	   //-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-	   //-0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
-	   // 0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f
+		20.0f, 20.0f, 20.0f, 1.0f, 1.0f, 1.0f,
+		10.0f, 10.0f, 10.0f, 0.0f, 1.0f, 0.0f
 	};
 
-	// generate VBO and VAO
+	int numVert = sizeof(vertices) / (sizeof(float) * 6);
+	cout << "Num vertices:" << numVert << endl;
+
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
-	// bind Vertex Array Object
 	glBindVertexArray(VAO);
-
-	// copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// color attribute
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	glm::mat4 skewMat = glm::mat4
-	(
-		1.0f, 0.0f, 0.9975f, 0.0f,
-		0.0f, 1.0f, 0.9975f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f
-	);
-
-	//glViewport(0, 0, 800, 600);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	// ----- 変更点 1: skewMat は不要なので削除 -----
+	// glm::mat4 skewMat = ...;
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -101,45 +90,78 @@ int main(void) {
 
 		ourShader.use();
 
-		// pass projection matrix to shader (note that in this case it could change every frame)
-		glm::mat4 projection = glm::perspective(glm::radians(90.0f), (float)WIN_W / (float)WIN_H, 2.0f, 100.0f);
+		// ----- 変更点 2: プロジェクション行列の生成方法を glm::frustum に変更 -----
+
+		// 1. 基本的なパラメータを設定
+		float fov = glm::radians(90.0f);
+		float aspectRatio = (float)WIN_W / (float)WIN_H;
+		float nearPlane = 2.0f;
+		float farPlane = 100.0f;
+
+		// 2. 対称なビューボリュームの境界を計算
+		float top = tan(fov / 2.0f) * nearPlane;
+		float bottom = -top;
+		float right = top * aspectRatio;
+		float left = -right;
+
+		// 3. 傾けたい角度からnearクリップ面でのズレ量を計算
+		float angleX = -20.0f / 20.0f;
+		float angleY = 20.0f / 20.0f;
+
+		float shiftX = nearPlane * angleX;
+		float shiftY = nearPlane * angleY;
+
+		// 4. 境界値にズレ量を加算
+		float finalLeft = left + shiftX;
+		float finalRight = right + shiftX;
+		float finalBottom = bottom + shiftY;
+		float finalTop = top + shiftY;
+
+		// 5. glm::frustumで斜めプロジェクション行列を生成
+		glm::mat4 projection = glm::frustum(finalLeft, finalRight, finalBottom, finalTop, nearPlane, farPlane);
 		ourShader.setMat4("projection", projection);
 
-		// camera/view transformation
-		glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f,0.1f,0.0f));
+		// --------------------------------------------------------------------
+
+		// camera/view transformation (変更不要)
+		// Note: カメラの上方向ベクトルは通常 (0,1,0) ですが、(0,0.1,0)でもlookAt内で正規化されるため動作します
+		glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		ourShader.setMat4("view", view);
 
-		glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		// model matrix (変更不要)
+		glm::mat4 model = glm::mat4(1.0f);
 		ourShader.setMat4("model", model);
 
 		ourShader.setFloat("pSize", 10.0f);
-		ourShader.setMat4("skew", skewMat);
+
+		// ----- 変更点 3: skewMatの送信コードは不要なので削除 -----
+		// ourShader.setMat4("skew", skewMat);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_POINTS, 0, 4);
+		glDrawArrays(GL_POINTS, 0, numVert); // 点は1つなので、個数を1に修正
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	// (以降の画像保存処理などは変更なし)
 	vector<unsigned char> buf(WIN_W * WIN_H * 3);
 	glReadBuffer(GL_FRONT);
 	glReadPixels(0, 0, WIN_W, WIN_H, GL_RGB, GL_UNSIGNED_BYTE, buf.data());
-	
+
 	cv::Mat img(WIN_H, WIN_W, CV_8UC3);
 	for (int y = 0; y < WIN_H; ++y) {
-	    unsigned char* dst = img.ptr<unsigned char>(WIN_H - 1 - y);
-	    const unsigned char* src = buf.data() + y * WIN_W * 3;
-	    for (int x = 0; x < WIN_W; ++x) {
-	        dst[x * 3 + 0] = src[x * 3 + 2]; // B
-	        dst[x * 3 + 1] = src[x * 3 + 1]; // G
-	        dst[x * 3 + 2] = src[x * 3 + 0]; // R
-	    }
+		unsigned char* dst = img.ptr<unsigned char>(WIN_H - 1 - y);
+		const unsigned char* src = buf.data() + y * WIN_W * 3;
+		for (int x = 0; x < WIN_W; ++x) {
+			dst[x * 3 + 0] = src[x * 3 + 2]; // B
+			dst[x * 3 + 1] = src[x * 3 + 1]; // G
+			dst[x * 3 + 2] = src[x * 3 + 0]; // R
+		}
 	}
-	
+
 	std::ostringstream stream;
-	stream << "./images/scratch/sample-skew.png";
+	stream << "./sample-oblique.png"; // 出力ファイル名を変更
 	std::string outPath = stream.str();
 
 	cv::imwrite(outPath, img);
